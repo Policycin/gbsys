ab3521857599adb9b8afc42d3aeca6a0d4d9ce5b
zxdfromaohan@users.noreply.github.com <729198387@qq.com>
2020-12-15 17:39:45 +0800

    构建项目

---
+++ gb.manage.system/src/test/java/com/manage/gb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.gb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
